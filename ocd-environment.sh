404ee64fda1732059c4588919e15794f7f7061ba
Your Name <you@example.com>
2018-11-26 22:47:17 +0000

    initial draft

--- ocd-environment.sh
+++ ocd-environment.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+oc() { 
+    ./oc_wrapper.sh "$@" 
+}
 find $(dirname $0) -name ocd-environment.yaml | while read YAML; do
   folder=$(realpath $(dirname $YAML))
   echo $folder
@@ -14,14 +17,22 @@ find $(dirname $0) -name ocd-environment.yaml | while read YAML; do
   # chartVersion: the version of the chart to use
   chartVersion=$(yq .chartVersion $YAML)
   echo chartVersion=${chartVersion}
-  # setEnvVarTemplate: how to map env vars into the helm set parameter
-  setEnvVarTemplate=$(yq .setEnvVarTemplate $YAML)
-  echo setEnvVarTemplate=${setEnvVarTemplate}
-  env=$folder/env  
-  set -a
-  [ -f $env ] && . $env
-  set +a
-  setParam=$(echo $setEnvVarTemplate | envsubst)
-  echo setParam=${setParam}
-  
+  # helmValuesFile: the file to load chart values from
+  helmValuesFile=$(yq .helmValuesFile $YAML)
+  echo helmValuesFile=${helmValuesFile}
+  # 
+  set -x
+  oc project realworld
+  helm init --client-only
+  helm repo list | grep ocd-meta > /dev/null
+  if [ "$?" != "0" ]; then
+    echo adding repo ocd-meta 
+    helm repo add ocd-meta https://ocd-scm.github.io/ocd-meta/charts
+  fi
+  helm upgrade --install \
+     -f ${helmValuesFile} \
+     --version ${chartVersion} \
+     --namespace realworld \
+     ${name} \
+     ${chart}
 done
