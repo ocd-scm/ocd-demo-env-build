d01c614650e7ba4bded977e03541e3dff68f2356
Your Name <you@example.com>
2018-11-27 19:52:34 +0000

    semver check. stripped quotes out of values

--- ocd-environment.sh
+++ ocd-environment.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+VERSION=1.0.0
 oc() { 
     ./oc_wrapper.sh "$@" 
 }
@@ -6,19 +7,24 @@ find $(dirname $0) -name ocd-environment.yaml | while read YAML; do
   folder=$(realpath $(dirname $YAML))
   echo $folder
   # ocdEnvVersion: version of ocd-environment-webhook this configuration is compatible with
-  ocdEnvVersion=$(yq .ocdEnvVersion $YAML)
+  ocdEnvVersion=$(yq .ocdEnvVersion $YAML | sed 's/"//g')
   echo ocdEnvVersion=${ocdEnvVersion}
+  ./semver_ge.bash $VERSION $ocdEnvVersion
+  if [ "$?" -ne "0" ]; then
+    >&2 echo "$VERSION < $ocdEnvVersion so we are too old for this configuration"
+    exit $?
+  fi
   # name: the name that helm should use
-  name=$(yq .name $YAML)
+  name=$(yq .name $YAML | sed 's/"//g')
   echo name=${name}
   # chart: the chart to install with
-  chart=$(yq .chart $YAML)
+  chart=$(yq .chart $YAML | sed 's/"//g')
   echo chart=${chart}
   # chartVersion: the version of the chart to use
-  chartVersion=$(yq .chartVersion $YAML)
+  chartVersion=$(yq .chartVersion $YAML | sed 's/"//g')
   echo chartVersion=${chartVersion}
   # helmValuesFile: the file to load chart values from
-  helmValuesFile=$(yq .helmValuesFile $YAML)
+  helmValuesFile=$(yq .helmValuesFile $YAML | sed 's/"//g')
   echo helmValuesFile=${helmValuesFile}
   # 
   set -x
