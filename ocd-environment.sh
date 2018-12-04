4573904ab40101d83cd448329b8473246ee3c55d
Simon Massey <simon@simonmassey.org>
2018-12-04 23:25:17 +0000

    helm init --client-only

--- ocd-environment.sh
+++ ocd-environment.sh
@@ -3,6 +3,10 @@ oc() {
     ./oc_wrapper.sh "$@" 
 }
 oc project $PROJECT
+helm repo list
+if [ "$?" != "0" ]; then
+  helm init --client-only  
+fi
 find $(dirname $0) -name helmfile.yaml | while read YAML; do
   folder=$(realpath $(dirname $YAML))
   echo $folder
