1b8b8e151c95e917b04df407d7d195748c67f3b5
Simon Massey <simbo1905@60hertz.com>
2018-12-04 01:14:08 +0000

    ocd-environment.sh

--- ocd-environment.sh
+++ ocd-environment.sh
@@ -3,6 +3,7 @@ VERSION=1.0.0
 oc() { 
     ./oc_wrapper.sh "$@" 
 }
+oc project $PROJECT
 find $(dirname $0) -name helmfile.yaml | while read YAML; do
   folder=$(realpath $(dirname $YAML))
   echo $folder
