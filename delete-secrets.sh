6d24fa072d981167ad89b80a7c37314ff8254be4
Simon Massey <simon@simonmassey.org>
2018-12-05 21:30:26 +0000

    delete secrets and extracted import key

--- delete-secrets.sh
+++ delete-secrets.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+# find all the the *secret files and delete the unecrypted version
+find . -type f -name '*secret' | while read -r SECRET ; do 
+    if [ -f "${SECRET%.*}" ]; then
+        rm "${SECRET%.*}"
+    fi
+done
--- reveal-secrets.sh
+++ reveal-secrets.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
+export GNUPGHOME=/opt/app-root/work/.gnupg/
+
 # this wrapper will detected session timeout and login
 oc() { 
     ./oc_wrapper.sh "$@" 
 }
 
+if [ ! -d /opt/app-root/work/.gnupg ]; then
+    ./import-gpg-key.sh
+fi
+
 oc project $PROJECT 
 
 PASSPHRASE=$(oc get secrets openshift-passphrase -o yaml | grep passphrase: | awk '{print $2}' | base64 --decode)
-
-if [ ! -d /opt/app-root/work/.gnupg ]; then
-    mkdir /opt/app-root/work/.gnupg
-    chmod og-wrx /opt/app-root/work/.gnupg
-    export GNUPGHOME=/opt/app-root/work/.gnupg/
-    KEY=$(find . -name ocd-private.key)
-    echo $PASSPHRASE | gpg --pinentry loopback --import --passphrase-fd 0 $KEY
-fi
 
 # gpg decrypt all the *secret files
 find . -type f -name '*secret' | while read -r SECRET ; do 
