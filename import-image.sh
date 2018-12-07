a5002cec741eca9299d5d602d72e56ca4633ca2f
Simon Massey <simon@simonmassey.org>
2018-12-07 20:28:35 +0000

    OCD_SCRIPTS_PATH

--- import-image.sh
+++ import-image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 oc() { 
-    ./oc_wrapper.sh "$@" 
+    ${OCD_SCRIPTS_PATH}/oc_wrapper.sh "$@" 
 }
 oc project $PROJECT
 oc import-image nodejs-8-rhel7:1-31 --from='registry.access.redhat.com/rhscl/nodejs-8-rhel7:1-31' --confirm
